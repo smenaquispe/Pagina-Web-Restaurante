ba025578cc14c009dda83eb1d1f553ee68e4584f
smenaq <sergiosantosmenaq@gmail.com>
2023-04-03 17:14:32 -0500

    try model but not working :(

--- backend/app/users/domain/user.ts
+++ backend/app/users/domain/user.ts
@@ -1,9 +1,19 @@
+
+import {Optional, Model, DataTypes} from 'sequelize'
 /**
  * in domain we create the entity, in this case user
  */
 
-export interface user {
-    id: string,
+interface UserAttributes {
+    id: number,
     email: string, 
-    password: string
+    passwd: string
+}
+
+export interface UserInput extends Required<UserAttributes> {}
+
+export class User extends Model<UserAttributes,UserInput> implements UserAttributes {
+    public id!: number
+    public email!: string 
+    public passwd!: string
 }
